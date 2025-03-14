38f0c907d60af731f87a1ad02f036f7cfaabb4de
Bismus <alexeybismus@gmail.com>
2025-03-14 13:02:08 +0300

    feat/wip: create core folder of app with error, util, usecase, constants

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,30 @@
+import '../constants/messages.dart';
+
+enum FailureType { serverError, connectionError, cacheError }
+
+class Failure {
+  final FailureType type;
+  final String message;
+
+  const Failure(this.type, this.message);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Failure && other.type == type;
+  }
+
+  @override
+  int get hashCode => type.hashCode;
+
+  static const Map<FailureType, String> _defaultMessages = {
+    FailureType.serverError: SERVER_FAILURE_MESSAGE,
+    FailureType.connectionError: CONNECTION_FAILURE_MESSAGE,
+    FailureType.cacheError: CACHE_FAILURE_MESSAGE,
+  };
+
+  factory Failure.fromType(FailureType type, {String? customMessage}) {
+    return Failure(type, customMessage ?? _defaultMessages[type]!);
+  }
+}
